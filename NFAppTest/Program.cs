7753b6e8ddeca9279367e0ded466e7146654d9e2
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2022-10-06 14:55:56 -0300

    Add project files.

---
+++ NFAppTest/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Device.Gpio;
+using System.Diagnostics;
+using System.Threading;
+
+namespace NFAppTest
+{
+    public class Program
+    {
+        private static GpioController s_GpioController;
+        public static void Main()
+        {
+            s_GpioController = new GpioController();
+
+            // ESP32 DevKit: 4 is a valid GPIO pin in, some boards like Xiuxin ESP32 may require GPIO Pin 2 instead.
+            GpioPin led = s_GpioController.OpenPin(
+            2,
+            PinMode.Output);
+
+            led.Write(PinValue.Low);
+
+            while (true)
+            {
+                led.Toggle();
+                Thread.Sleep(125);
+                led.Toggle();
+                Thread.Sleep(125);
+                led.Toggle();
+                Thread.Sleep(125);
+                led.Toggle();
+                Thread.Sleep(525);
+            }
+        }
+    }
+}
